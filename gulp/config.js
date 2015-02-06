8805c65022664a22bd7978c8e34ee7317e002132
Ivan Fonin <mail@ivanfonin>
2015-02-06 09:48:19 +0000

    Added new tasks and theme files

--- gulp/config.js
+++ gulp/config.js
@@ -1,12 +1,11 @@
 /*
-	config.js
-	=========
-	Configuration file.
+config.js
+=========
+Gulp configuration file for the Themegulper workflow.
 */
 
-// Project paths configuration
-var theme   = "starter",                 // WordPress theme name goes here 
-    source  = "./source",                // Source files are here, edit only in this directory
-    build   = "./build",                 // Working WordPress theme for testing purposes
-    dist    = "./dist" + theme + "/";    // Distribution version of the WordPress Theme will be here
-    
+// Project paths variables
+var theme  = "starter", // Name of your WordPress theme
+    source = "./source", // Source files are here, edit only in this directory
+    build  = "./build", // Working WordPress theme for testing purposes
+    dist   = "./dist" + theme + "/"; // Distribution version of the theme
