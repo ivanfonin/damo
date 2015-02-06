e3ebbf75ed5d6a70b0264c2b0aef18e9b6642bff
Ivan Fonin <mail@ivanfonin>
2015-02-06 06:52:18 +0000

    Added 'gulp/config.js' file

---
+++ gulp/config.js
@@ -0,0 +1,12 @@
+/*
+	config.js
+	=========
+	Configuration file.
+*/
+
+// Project paths configuration
+var theme   = "starter",                 // WordPress theme name goes here 
+    source  = "./source",                // Source files are here, edit only in this directory
+    build   = "./build",                 // Working WordPress theme for testing purposes
+    dist    = "./dist" + theme + "/";    // Distribution version of the WordPress Theme will be here
+    
